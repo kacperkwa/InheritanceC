0c6555db1568bf0fa499a645ab14cf07ef4fcb35
kacperkwa <kacperkwartaxz@gmail.com>
2023-06-30 13:52:16 +0200

    Add project files.

---
+++ InheritanceC/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InheritanceC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
