56ecb1325c8630075b1990d60af5367933e1bcfe
kacperkwa <kacperkwartaxz@gmail.com>
2023-06-30 14:33:19 +0200

    Added ImagePost class which inherits some data from Post class.

---
+++ InheritanceC/ImagePost.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceC
+{
+    internal class ImagePost:Post
+    {
+        public string ImageURL { get; set; }
+
+        public ImagePost()
+        {
+            
+        }
+
+        public ImagePost(string title,string sendByUsername,string imageURL,bool isPublic)
+        {
+            //the following properties and the GetNextID method are inherited from Post.
+            this.ID=GetNextID();
+            this.Title=title;
+            this.SendByUsername=sendByUsername;
+            this.IsPublic=isPublic;
+            //Property ImageUrl is a member of ImagePost,but not of Post
+            this.ImageURL = imageURL;
+        }
+        public override string ToString()
+        {
+            return String.Format($"{this.ID} - {this.Title} - {this.ImageURL}- {this.SendByUsername}");
+        }
+    }
+}
--- InheritanceC/Program.cs
+++ InheritanceC/Program.cs
@@ -6,9 +6,10 @@
         {
             Post post1 = new Post();
             Post post2 = new Post("Thanks for the birthday wishes", true, "FunnyName");
-            Post post3= new Post("Hi everyone!",false,"C#DevWithNoJob");
+            Post post3= new Post("Hi everyone!",true,"C#DevWithNoJob");
             Console.WriteLine(post3.ToString());
-
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes","SneekersLover","https://image.com/shoes",true);
+            Console.WriteLine(imagePost1.ToString());
             Console.ReadLine();
         }
     }
